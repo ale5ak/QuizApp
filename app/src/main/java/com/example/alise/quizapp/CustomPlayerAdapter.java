e98e0df17e46cde22521047eca93e6496b33a3af
Aleš Pros <Ale Pros>
2017-03-26 17:28:40 +0200

    Changed design of the list item

--- app/src/main/java/com/example/alise/quizapp/CustomPlayerAdapter.java
+++ app/src/main/java/com/example/alise/quizapp/CustomPlayerAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.alise.quizapp.data.ScoreContract.ScoreEntry;
@@ -32,10 +33,23 @@ public class CustomPlayerAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         TextView playerName = (TextView) view.findViewById(R.id.player_name);
         TextView playerScore = (TextView) view.findViewById(R.id.player_score);
+        TextView playerScoreBarLeft = (TextView) view.findViewById(R.id.player_score_bar_left);
+        TextView playerScoreBarRight = (TextView) view.findViewById(R.id.player_score_bar_right);
 
         String playerNameValue = cursor.getString(cursor.getColumnIndexOrThrow(ScoreEntry.COLUMN_NAME));
         int playerScoreInt = cursor.getInt(cursor.getColumnIndexOrThrow(ScoreEntry.COLUMN_SCORE));
-        String playerScoreValue = "" + (playerScoreInt * 100 / 4) + " %";
+        int playerScorePercent = playerScoreInt * 100 / 4;
+        String playerScoreValue = "" + playerScorePercent + " %";
+
+        //Set up the left side of percentage bar based on the points achieved
+        LinearLayout.LayoutParams paramsScoreBarLeft = (LinearLayout.LayoutParams) playerScoreBarLeft.getLayoutParams();
+        paramsScoreBarLeft.weight = playerScorePercent;
+        playerScoreBarLeft.setLayoutParams(paramsScoreBarLeft);
+
+        //Set up the right side of percentage bar based on the points achieved
+        LinearLayout.LayoutParams paramsScoreBarRight = (LinearLayout.LayoutParams) playerScoreBarRight.getLayoutParams();
+        paramsScoreBarRight.weight = 100 - playerScorePercent;
+        playerScoreBarRight.setLayoutParams(paramsScoreBarRight);
 
         playerName.setText(playerNameValue);
         playerScore.setText(playerScoreValue);
